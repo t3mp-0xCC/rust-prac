06c1ecafa230d422876b047a334e41dda1b06e00
t3mp-0xCC <kurose_kiy@cma.bbiq.jp>
2021-07-20 01:58:12 +0900

    add TODO

--- debugger/src/main.rs
+++ debugger/src/main.rs
@@ -1,20 +1,22 @@
 use std::{env, process::exit};
-
-fn init() {
-    // arg check
-    let args: Vec<String> = env::args().collect();
-
-    if args.len() < 2{
-            print_help();
-            exit(-1);
-    }
-}
+use std::path::Path;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn print_help() {
     println!("Usage: debugger <exec bin>");
 }
 
 fn main() {
-    init();
-    println!("temporary debugger\n written by Rust!");
+     // arg check
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2{
+            print_help();
+            exit(-1);
+    }
+    // TODO: file check
+
+    
+    println!("temporary debugger\nwritten by Rust!");
 }
