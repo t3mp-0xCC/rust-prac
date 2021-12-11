acf33f7b7cbb1554e1a95f0ae6dfb33fcc4be412
t3mp-0xCC <40132263+t3mp-0xCC@users.noreply.github.com>
2021-12-12 03:28:22 +0900

    roulette: minimal implementation

--- roulette/src/main.rs
+++ roulette/src/main.rs
@@ -1,3 +1,34 @@
+use std::{
+    env,
+    fs::File,
+    io::{self, BufRead, BufReader},
+    path::Path,
+    fmt::Display
+};
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    /*TODO: if argc != 1, show help*/
+    // args
+    let args: Vec<String> = env::args().collect();
+    let path = Path::new(&args[1]);
+    // reading file and get elements as vec
+    let roulette_elements = lines_from_file(path)
+        .expect("file not found !");
+    // show elements of file
+    println!("Roulette of Elements");
+    let mut i = 1;
+    for element in roulette_elements.iter() {
+        println!("{}: {:?}",i , element);
+        i+=1;
+    }
+    // roulette time !
+    let mut rand_num = rand::thread_rng().gen_range(1, roulette_elements.len());
+    println!("***{:?}***", roulette_elements[rand_num]);
+
+}
+
+// reading file and split line into vec
+fn lines_from_file(file: impl AsRef<Path>) -> io::Result<Vec<String>> {
+    BufReader::new(File::open(file)?).lines().collect()
 }
