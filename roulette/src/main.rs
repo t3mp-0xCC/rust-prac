524a32e90a840940116d031c35b2f5453409898b
t3mp-0xCC <40132263+t3mp-0xCC@users.noreply.github.com>
2021-12-13 04:27:49 +0900

    roulette: show help

--- roulette/src/main.rs
+++ roulette/src/main.rs
@@ -8,13 +8,18 @@ use std::{
 use rand::Rng;
 
 fn main() {
-    /*TODO: if argc != 1, show help*/
-    // args
+    // show help
     let args: Vec<String> = env::args().collect();
-    let path = Path::new(&args[1]);
+    if args.len() != 2 {
+        println!("Usage: roulette <elements file>\nexp: roulette foods.txt");
+        return;
+    }
+
     // reading file and get elements as vec
+    let path = Path::new(&args[1]);
     let roulette_elements = lines_from_file(path)
         .expect("file not found !");
+
     // show elements of file
     println!("Roulette of Elements");
     let mut i = 1;
@@ -22,6 +27,7 @@ fn main() {
         println!("{}: {:?}",i , element);
         i+=1;
     }
+
     // roulette time !
     let mut rand_num = rand::thread_rng().gen_range(1, roulette_elements.len());
     println!("***{:?}***", roulette_elements[rand_num]);
